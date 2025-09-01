51d546591230aa1498b50c61d8ad45b4ec87eaa7
Nick.Brigden <nick.brigden@bjss.com>
2025-09-01 15:01:08 +0100

    Added capture for icon-size

--- src/l-div-icon.test.js
+++ src/l-div-icon.test.js
@@ -7,9 +7,30 @@ it("should render a div icon", () => {
   const el = document.createElement("l-div-icon");
   el.innerHTML = "Hello, World!";
   el.setAttribute("icon-anchor","[50, 50]");
+  el.setAttribute("icon-size", "[100, 100]")
   document.body.appendChild(el);
   expect(el.icon).toBeInstanceOf(DivIcon);
-  expect(el.icon).toEqual(divIcon({ html: "Hello, World!" , iconAnchor: "[50, 50]"}));
+  expect(el.icon).toEqual(divIcon({ html: "Hello, World!" , iconAnchor: [50, 50], iconSize: [100, 100]}));
+});
+
+it("should assign null as iconSize when null passed", () => {
+  const el = document.createElement("l-div-icon");
+  el.innerHTML = "Hello, World!";
+  el.setAttribute("icon-anchor","[50, 50]");
+  el.setAttribute("icon-size","null");
+  document.body.appendChild(el);
+  expect(el.icon).toBeInstanceOf(DivIcon);
+  expect(el.icon).toEqual(divIcon({ html: "Hello, World!" , iconAnchor: [50, 50], iconSize: null}));
+});
+
+it("should have a default iconSize if not given", () => {
+  const el = document.createElement("l-div-icon");
+  el.innerHTML = "Hello, World!";
+  el.setAttribute("icon-anchor","[50, 50]");
+  document.body.appendChild(el);
+  expect(el.icon).toBeInstanceOf(DivIcon);
+  expect(el.icon).toEqual(divIcon({ html: "Hello, World!" , iconAnchor: [50, 50]}));
+  expect(el.icon.options).toHaveProperty('iconSize', [12, 12]);
 });
 
 it("should attach div icon to marker", () => {
@@ -19,6 +40,5 @@ it("should attach div icon to marker", () => {
   marker.appendChild(icon);
   document.body.appendChild(marker);
 
-  const actual = marker.layer.getIcon();
   expect(icon.icon).toEqual(marker.layer.getIcon());
 });
